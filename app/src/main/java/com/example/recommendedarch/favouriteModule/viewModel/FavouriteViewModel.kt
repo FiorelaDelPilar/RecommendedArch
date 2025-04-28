252ce8be96907540dca952700aca5e1ae2dd2fa5
fvargasi <fvargasi@emeal.nttdata.com>
2025-04-28 14:25:57 -0500

    ViewModel Layer Favoritos

--- app/src/main/java/com/example/recommendedarch/favouriteModule/viewModel/FavouriteViewModel.kt
+++ app/src/main/java/com/example/recommendedarch/favouriteModule/viewModel/FavouriteViewModel.kt
@@ -1,14 +1,8 @@
 package com.example.recommendedarch.favouriteModule.viewModel
 
-import androidx.lifecycle.viewModelScope
-import com.example.recommendedarch.R
 import com.example.recommendedarch.common.entities.Wine
-import com.example.recommendedarch.common.utils.Constants
 import com.example.recommendedarch.common.viewModel.BaseWineViewModel
 import com.example.recommendedarch.favouriteModule.model.FavouriteRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class FavouriteViewModel(private val repository: FavouriteRepository) : BaseWineViewModel() {
     init {
@@ -16,55 +10,17 @@ class FavouriteViewModel(private val repository: FavouriteRepository) : BaseWine
     }
 
     override fun getAllWines() {
-        viewModelScope.launch {
-            setInProgress(Constants.SHOW)
-            try {
-                withContext(Dispatchers.IO) {
-                    val result = repository.getAllWines()
-                    result?.let { setWines(it) }
-                    if (result == null) {
-                        setSnackbarMsg(R.string.room_request_fail)
-                    }
-                }
-            } finally {
-                setInProgress(Constants.HIDE)
+        executeAction {
+            repository.getAllWines { wines ->
+                setWines(wines)
             }
         }
     }
 
-    override fun addWine(wine: Wine) {
-        viewModelScope.launch {
-            setInProgress(Constants.SHOW)
-            try {
-                withContext(Dispatchers.IO) {
-                    val result = repository.addWine(wine)
-                    if (result != -1L) {
-                        setSnackbarMsg(R.string.room_save_success)
-                    } else {
-                        setSnackbarMsg(R.string.room_save_fail)
-                    }
-                }
-            } finally {
-                setInProgress(Constants.HIDE)
-            }
-        }
-    }
-
-    fun deleteWine(wine: Wine) {
-        viewModelScope.launch {
-            setInProgress(Constants.SHOW)
-            try {
-                withContext(Dispatchers.IO) {
-                    val result = repository.deleteWine(wine)
-                    if (result == 0) {
-                        setSnackbarMsg(R.string.room_save_fail)
-                    } else {
-                        setSnackbarMsg(R.string.room_save_success)
-
-                    }
-                }
-            } finally {
-                setInProgress(Constants.HIDE)
+    fun updateFavourite(wine: Wine) {
+        executeAction {
+            repository.updateFavourite(wine) { resMsg ->
+                setSnackbarMsg(resMsg)
             }
         }
     }
