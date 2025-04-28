51f405889e99ea6548d8b4508b0bb427d0d2fec2
fvargasi <fvargasi@emeal.nttdata.com>
2025-04-28 18:52:17 -0500

    viewmodel ACCOUNT

--- app/src/main/java/com/example/recommendedarch/accountModule/viewModel/AccountViewModel.kt
+++ app/src/main/java/com/example/recommendedarch/accountModule/viewModel/AccountViewModel.kt
@@ -2,21 +2,11 @@ package com.example.recommendedarch.accountModule.viewModel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.recommendedarch.R
 import com.example.recommendedarch.accountModule.model.AccountRepository
 import com.example.recommendedarch.common.entities.FirebaseUser
-import com.example.recommendedarch.common.utils.Constants
-import kotlinx.coroutines.launch
+import com.example.recommendedarch.common.viewModel.BaseViewModel
 
-class AccountViewModel(private val repository: AccountRepository) : ViewModel() {
-    private val _inProgess = MutableLiveData<Boolean>()
-    val inProgess: LiveData<Boolean> = _inProgess
-
-    private val _snackbarMsg = MutableLiveData<Int>()
-    val snackbarMsg: LiveData<Int> = _snackbarMsg
-
+class AccountViewModel(private val repository: AccountRepository) : BaseViewModel() {
     private val _user = MutableLiveData<FirebaseUser?>()
     val user: LiveData<FirebaseUser?> = _user
 
@@ -28,28 +18,20 @@ class AccountViewModel(private val repository: AccountRepository) : ViewModel() 
     }
 
     private fun getCurrentUser() {
-        viewModelScope.launch {
-            _inProgess.value = Constants.SHOW
-            try {
-                _user.value = repository.getCurrentUser()
-            } catch (e: Exception) {
-                _snackbarMsg.value = R.string.account_sign_out_fail
-            } finally {
-                _inProgess.value = Constants.HIDE
+        executeAction {
+            repository.getCurrentUser { result ->
+                _user.value = result
             }
         }
     }
 
     fun singOut() {
-        viewModelScope.launch {
-            _inProgess.value = Constants.SHOW
-            try {
-                _isSignOut.value = repository.signOut()
-            } catch (e: Exception) {
-                _snackbarMsg.value = R.string.account_request_user_fail
-            } finally {
-                _inProgess.value = Constants.HIDE
+        executeAction {
+            repository.signOut { result ->
+                _isSignOut.value = result
             }
         }
     }
+
+    override fun onPause() = clearValues()
 }
