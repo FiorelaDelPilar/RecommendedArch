65de22ef629f0fa0dfdbc512383924c1fed149dc
fvargasi <fvargasi@emeal.nttdata.com>
2025-04-28 19:18:51 -0500

    fix ACCOUNT MODULE VIEWMODEL

--- app/src/main/java/com/example/recommendedarch/accountModule/di/AccountModule.kt
+++ app/src/main/java/com/example/recommendedarch/accountModule/di/AccountModule.kt
@@ -3,10 +3,11 @@ package com.example.recommendedarch.accountModule.di
 import com.example.recommendedarch.accountModule.model.AccountRepository
 import com.example.recommendedarch.accountModule.model.domain.AccountAuth
 import com.example.recommendedarch.accountModule.viewModel.AccountViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val accountModule = module {
     single { AccountAuth(get()) }
     factory { AccountRepository(get()) }
-    factory { AccountViewModel(get()) }
+    viewModel { AccountViewModel(get()) }
 }
